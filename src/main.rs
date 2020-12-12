1a1bc511eca9358f7850faeb00e58e244b4df2a6
Wyatt Turner <wyatturner.45@gmail.com>
2020-12-12 16:14:38 -0700

    Implemented callback from timer
    I have been trying to hook up a way so that the timer itself can emit events
    back to my GUI. Although I became aware of a reasonable issue. GTK objects
    aren't thread safe. Looks like in order for me to be able to update labels
    i'll need to use some mutexes. This learning process has become a mess of threads.

--- src/main.rs
+++ src/main.rs
@@ -21,6 +21,8 @@ fn main() {
         .expect("Could not load resources");
     gio::resources_register(&res);
 
+
+
     let app = gtk::Application::new(Some("org.example.App"), Default::default()).unwrap();
     app.connect_activate(move |app| {
         buildUI(&app);
@@ -37,6 +39,7 @@ fn main() {
 
 fn buildUI(application: &gtk::Application)
 {
+
     let builder = gtk::Builder::new_from_resource("/org/example/App/window.ui");
 
     let window : gtk::ApplicationWindow = builder.get_object("window").expect("Failed to get window");
@@ -44,6 +47,10 @@ fn buildUI(application: &gtk::Application)
     let description : gtk::Label = builder
         .get_object("descriptionLabel")
         .expect("Failed to get descriptionLabel");
+
+    let countdown : gtk::Label = builder
+        .get_object("timeLabel")
+        .expect("Failed to get timeLabel");
 
     let play_button: gtk::Button = builder
         .get_object("play_button")
@@ -53,7 +60,7 @@ fn buildUI(application: &gtk::Application)
         .get_object("reset_button")
         .expect("Failed to get resetButton");
 
-    createListeners(&play_button, &reset_button);
+    createListeners(&play_button, &reset_button, &description, &countdown);
 
 
     window.set_application(Some(application));
@@ -61,40 +68,71 @@ fn buildUI(application: &gtk::Application)
     window.present();
 }
 
-fn createListeners(play_button : &gtk::Button, reset_button : &gtk::Button)
+fn createListeners(play_button : &gtk::Button,
+                  reset_button : &gtk::Button,
+                  description : &gtk::Label,
+                  countdown : &gtk::Label)
 {
     let (messenger, receiver) = mpsc::channel();   //Will send messages to timer when play, reset
 
     let messenger_clone = mpsc::Sender::clone(&messenger);
+    //let play_clone = play_button.clone();
+    //let reset_clone = reset_button.clone();
+    //let descr_clone = description.clone();
+    //let countdown_clone = countdown.clone();
+
 
     play_button.connect_clicked(move |_| {
 
         messenger_clone.send("play");
-        //desc_clone.set_text("From play_button");
     });
 
 
     reset_button.connect_clicked(move |_| {
         messenger.send("reset");
-        //desc_clone.set_text("From reset _button");
     });
 
     thread::spawn(move || {
 
         let mut counter = Arc::new(Mutex::new(timer::Timer::new()));
-        //counter.initiateTimer();
-        timer::timerStart(counter.clone());
 
+        //timerListener(counter.clone(), &descr_clone, &countdown_clone);
+
+        timerListener(counter.clone());
+
+        //message is event from user clicking a button
         for message in receiver {
+
+            //play_clone.set_label(message);
 
             let mut timer_copy = counter.lock().unwrap();
 
             timer_copy.messageSent(message);
-            //println!("This happens {}", message);
 
         }
 
     });
+}
 
-    println!("There");
+//fn timerListener(timer : Arc<Mutex<Timer>>, timerEvents : mpsc::Receiver<&str>)
+/*
+fn timerListener(clock_timer : Arc<Mutex<timer::Timer>>,
+                desription : &gtk::Label,
+                countdown : &gtk::Label)
+                */
+fn timerListener(clock_timer : Arc<Mutex<timer::Timer>>)
+{
+    let (messenger, receiver) = mpsc::channel::<std::string::String>();     //Message is a String type as a hackish way to get around lifetime issues with using &str
+
+    let timerEvents = timer::timerStart(clock_timer, messenger);
+
+    thread::spawn(move || {
+
+        for message in receiver
+        {
+            println!("Message from timer {}", message);
+        }
+
+    });
+
 }
--- src/timer/mod.rs
+++ src/timer/mod.rs
@@ -1,10 +1,13 @@
 use std::thread;
 use std::time::Duration;
 use std::sync::{Arc, Mutex};
+use std::sync::mpsc;
 
 pub struct Timer
 {
     count : i32,
+    sprintCount: u32,
+    isBreak: bool,
     //isCounting : Arc<Mutex<bool>>,
     isCounting : bool,
 }
@@ -19,6 +22,8 @@ impl Timer
 
         Self {
             count : 100,
+            sprintCount: 0,
+            isBreak: false,
             //isCounting: Arc::new(Mutex::new(false)),
             isCounting : false,
         }
@@ -48,38 +53,24 @@ impl Timer
 
     }
 
-    pub fn initiateTimer(self : &mut Self)
-    {
-
-
-        thread::spawn(|| {
-            //self.timer();
-        });
-    }
-
-    //Actual timer updating the clock
-    fn timer(self : &mut Self)
-    {
-        loop{
-            thread::sleep(Duration::from_millis(1000));
-            /*
-            let isCounting = self.isCounting.lock().unwrap();
-            if(*isCounting)
-            {
-                //Self.count-=1;
-            }
-            */
-            println!("Timer ran");
-        }
-
-    }
 
 }
 
 //Experimental function, pass in ARC of Timer class to initiate things
-pub fn timerStart(timer : Arc<Mutex<Timer>>)
+//pub fn timerStart(timer : Arc<Mutex<Timer>>, messenger: mpsc::Sender<&str>) -> mpsc::Receiver<&str>
+pub fn timerStart(timer : Arc<Mutex<Timer>>, messenger: mpsc::Sender<std::string::String>)
+{
+    //let (messenger, receiver) = mpsc::channel();
+
+    countDown(timer, messenger);
+
+    //return receiver;
+}
+
+fn countDown(timer : Arc<Mutex<Timer>>, messenger: mpsc::Sender<std::string::String>)
 {
     thread::spawn(move || {
+
         loop{
             thread::sleep(Duration::from_millis(1000));
 
@@ -89,6 +80,7 @@ pub fn timerStart(timer : Arc<Mutex<Timer>>)
             {
                 timer_copy.count -=1;
                 println!("Is counting.. {}", timer_copy.count);
+                messenger.send(timer_copy.count.to_string());
             }
 
             println!("Timer ran");
@@ -96,17 +88,6 @@ pub fn timerStart(timer : Arc<Mutex<Timer>>)
             }
 
       });
-}
-
-pub fn testing()
-{
-
-    println!("From within timer!");
-}
-
-pub fn what() -> Timer
-{
-
-    return Timer::new();
 
 }
+
